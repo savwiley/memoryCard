a827ac0a57e7ce04fe58109ea688d6ce2e28be41
Savannaha <savwiley@yahoo.com>
2021-04-03 16:31:17 -0500

    added credit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Container from "./components/container.js";
-//import ScoreBoard from "./components/scoreboard.js";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const App = () => {
   return (
     <div className="main">
 
       <div className="header">
+        <a href="https://github.com/savwiley/memoryCard" id="credit">
+          <FontAwesomeIcon icon={faGithub} id="credit" />
+        </a>
         <span>Marvel-ous Memory</span>
       </div>
 
@@ -21,3 +25,17 @@ const App = () => {
 }
 
 export default App;
+
+
+
+/**
+ * TO-DO
+ * 
+ * Run prettier
+ * Insert gh-pages commands
+ * Actually deploy it
+ * Make license
+ * Edit README
+ * -- credit fa, google fonts, the site for the posters, the so func, react, prettier, gh-pages
+ * Clear out scoreboard.js and card.js - Delete them from gh
+ */
